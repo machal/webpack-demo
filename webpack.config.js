bc223bfbdfaedcd6b8b685f99b67e1892213ef9c
Martin Michálek <michalek@shortcat.cz>
2019-10-08 03:00:48 +0200

    Sass -> CSS - inserting into JS file

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,17 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main-configured.js"
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }  
 };
