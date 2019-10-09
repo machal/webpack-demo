d272a7b30a07a2fd5c9137a29ff29a6097c35a44
Martin Michálek <michalek@shortcat.cz>
2019-10-09 06:31:08 +0200

    Saving SCSS into CSS file

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-  entry: "./src/index.js",
+  entry: ["./src/index.js", "./src/styles.scss"],
   output: {
     filename: "main-configured.js"
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader",
+          "sass-loader"
         ]
       }
     ]
-  }  
+  }
 };
